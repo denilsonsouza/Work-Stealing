ee4b54aad39e7474914a2be83ac5235431600f5c
Denilson Souza <denilson.souza.o@gmail.com>
2018-09-18 11:11:26 -0300

    configurando o CMake e adicionando pastas

---
+++ src/build/CMakeFiles/ws_test_task.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/denilson/Documentos/Work-Stealing/src/ws_test_task.c" "/home/denilson/Documentos/Work-Stealing/src/build/CMakeFiles/ws_test_task.dir/ws_test_task.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/mpich"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/build/CMakeFiles/ws_test_task.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ws_test_task.dir/ws_test_task.c.o"
+  "ws_test_task.pdb"
+  "ws_test_task"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ws_test_task.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
